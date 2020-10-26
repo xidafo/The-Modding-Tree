89f91a52d1824ed57d58405234e0cb9356d5c77d
xidafo <73483271+xidafo@users.noreply.github.com>
2020-10-26 19:27:19 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,6 @@ addLayer("w", {
         exponent: 0.5, // Prestige currency exponent
         gainMult() { // Calculate the multiplier for main currency from bonuses
             mult = new Decimal(1)
-            if (player.p.upgrades.includes(11)) mult = mult.times(2)
             return mult
         },
         gainExp() { // Calculate the exponent on main currency from bonuses
